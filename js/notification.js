ca81c8c825b772fd6d7ecad012c27fe0b4422da2
Mayron Cachina <mayroncachina@gmail.com>
2014-06-10 22:28:37 -0300

    implementacao do webservice notificacao

--- js/notification.js
+++ js/notification.js
@@ -3,11 +3,23 @@ setInterval(function(){
 
 	$.ajax({
 	    type: 'GET',
-	    url: "http://107.170.191.250:8080/webservices/bares/num/",
+	    url: "http://www.wheresbar.com.br:8080/webservices/push/",
 	    dataType: 'text',
 	    crossDomain: true,
 	    success: function(data, ts, xhr){
-	    	alert(data)
+
+
+
+	    	for (var i = 0; i < data.promocoes.length; i++) {
+	    		alert(a.promocoes[i][1]);
+	    		/*
+				window.plugin.notification.local.add({
+				    id:         1,
+				    message:    'I love BlackBerry!',
+				    json:       JSON.stringify({ test: 123 })
+				});
+				*/
+	    	};
 	/*
 			window.plugin.notification.local.add({
 			    id:         1,
