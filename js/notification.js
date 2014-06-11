c017c5216e23b127170ead9af51622128dd4eb8f
Mayron Cachina <mayroncachina@gmail.com>
2014-06-10 21:41:09 -0300

    teste de notification

---
+++ js/notification.js
@@ -0,0 +1,21 @@
+
+setInterval(function(){
+
+	$.ajax({
+	    type: 'GET',
+	    url: "http://107.170.191.250:8080/webservices/bares/num/",
+	    dataType: 'text',
+	    crossDomain: true,
+	    success: function(data, ts, xhr){
+	    	console.log(data)
+	/*
+			window.plugin.notification.local.add({
+			    id:         1,
+			    message:    'I love BlackBerry!',
+			    json:       JSON.stringify({ test: 123 })
+			});
+	*/
+	    }
+	});
+
+}, 1000);
